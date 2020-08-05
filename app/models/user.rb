8ba31713c7657b796c94435c5d4c40638f2d09b6
Pablo Curell <pablocm90@gmail.com>
2020-08-05 20:35:58 +0200

    [ADD] usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :user_hints
   has_many :hints, through: :user_hints
+  validates :username, presence: true
 
   def next_hint
     last_answered = hints.order(order: :asc).last&.order.to_i
