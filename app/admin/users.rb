28e3ce11891d428e0dc2220dda63550efd3cacd7
Pablo Curell <pablocm90@gmail.com>
2020-08-30 11:22:20 +0200

    .

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -6,6 +6,14 @@ ActiveAdmin.register User do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :username, :group_id
+
+  form do |f|
+    f.input :group_id
+    f.input :email
+    f.input :username
+  end
+
+  
   #
   # or
   #
