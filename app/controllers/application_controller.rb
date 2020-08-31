f4f7d253040eaaaca333c5994b2127420e9b63f3
Pablo Curell <pablocm90@gmail.com>
2020-08-31 16:32:47 +0200

    group id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username group])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username group_id])
   end
 end
